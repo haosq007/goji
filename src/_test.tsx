f5078efd27da6d4ae7a927669c61a53e0dd698a7
hao.shiqi <HaoShiQi_1059594@wdai.com>
2023-03-24 15:55:57 +0800

    feat：ahctGpt自动生成代码还请慢慢欣赏，告辞

--- src/_test.tsx
+++ src/_test.tsx
@@ -1,17 +1,25 @@
+import React, { useState } from "react";
+import ReactDom from "react-dom";
+import Modal from "./components/modal/modal";
+import Upload from "./components/upload/upload";
+import Table from "./components/table/table";
+import Tab from "./components/tab/tab";
 
-import React, { useState } from 'react';
-import ReactDom from 'react-dom'
-import Modal from './components/modal/modal';
-import Upload from './components/upload/upload';
-import Tab from './components/tab/tab';
-
-import * as GOJI from 'goji_ui'
+import * as GOJI from "goji_ui";
 
 function App() {
-	const [visible, setVisible] = useState(false)
-	const [ev, setEv] = useState(false)
-	return <div>
-		{/* <GOJI.Tab
+  const [visible, setVisible] = useState(false);
+  const [ev, setEv] = useState(false);
+  const data = [
+    { id: 1, name: "Alice", age: 20 },
+    { id: 2, name: "Bob", age: 25 },
+    { id: 3, name: "Charlie", age: 30 },
+  ];
+
+  const col = ["id", "name", "age"];
+  return (
+    <div>
+      {/* <GOJI.Tab
 			extSelector={'.tab'}
 			extension={<div>这是扩展的内容</div>}
 			items={[
@@ -28,76 +36,85 @@ function App() {
 			]}
 		/> */}
 
+      <h1>test</h1>
+      <button
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        show modal
+      </button>
+      <Tab
+        onTabChange={() => {
+          setEv(true);
+        }}
+        hiddenStyle={{
+          height: "0px",
+          overflow: "hidden",
+        }}
+        tabContentVisible={ev}
+        extSelector={'[aria-label="tab"]'}
+        extension={
+          <div
+            onClick={() => {
+              setEv(!ev);
+            }}
+            className="ext"
+          >
+            这是扩展的内容
+          </div>
+        }
+        items={[
+          {
+            title: "tab1",
+            key: "tab1",
+            children: <div>tab1</div>,
+          },
+          {
+            title: "tab2",
+            key: "tab2",
+            children: <div>tab2</div>,
+          },
+        ]}
+      />
 
-		<h1>test</h1>
-		<button
-			onClick={() => {
-				setVisible(true)
-			}}
-		>
-			show modal
-		</button>
-		<Tab
-			onTabChange={()=>{
-				setEv(true)
-			}}
-			hiddenStyle={{
-				height:'0px',
-				overflow:'hidden'
-			}}
-			tabContentVisible={ev}
-			extSelector={'[aria-label="tab"]'}
-			extension={<div onClick={() => { setEv(!ev) }} className="ext">这是扩展的内容</div>}
-			items={[
-				{
-					title: "tab1",
-					key: "tab1",
-					children: <div>tab1</div>
-				},
-				{
-					title: "tab2",
-					key: "tab2",
-					children: <div>tab2</div>
-				}
-			]}
-		/>
+      <Modal
+        // dontDestroyOnClose={true}
+        onClose={() => {
+          setVisible(false);
+        }}
+        visible={visible}
+      >
+        <div>
+          how to set default value for typescript interface field
+          <input type="text" />
+        </div>
+      </Modal>
 
-		<Modal
-			// dontDestroyOnClose={true}
-			onClose={() => {
-				setVisible(false)
-			}}
-			visible={visible}
-		>
-			<div>
-				how to set default value for typescript interface field
-				<input type="text" />
-			</div>
-		</Modal>
-
-		<Upload
-			uploadUrl='/api/video-service/upload'
-			beforeUpload={(f) => {
-				for (var i = 0; i < f.length; i++) {
-					console.log(f[i].name)
-				}
-				return new Promise((r, j) => {
-					setTimeout(() => {
-						r(f)
-					}, 1000);
-				})
-			}}
-
-			valueFilter={({ response }) => {
-				return (response as Record<string, Object>).url
-			}}
-			onComplete={(res: any[]) => {
-				console.log(res)
-			}}
-		>
-			请选择文件
-		</Upload>
-	</div >
+      <Upload
+        uploadUrl="/api/video-service/upload"
+        beforeUpload={(f) => {
+          for (var i = 0; i < f.length; i++) {
+            console.log(f[i].name);
+          }
+          return new Promise((r, j) => {
+            setTimeout(() => {
+              r(f);
+            }, 1000);
+          });
+        }}
+        valueFilter={({ response }) => {
+          return (response as Record<string, Object>).url;
+        }}
+        onComplete={(res: any[]) => {
+          console.log(res);
+        }}
+      >
+        请选择文件
+      </Upload>
+      <Table data={data} col={col} />
+    </div>
+  );
 }
 
-ReactDom.render(<App />, document.getElementById("app"))
+ReactDom.render(<App />, document.getElementById("app"));
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,52 +1,71 @@
-
-import React, { ReactElement, useMemo } from 'react';
-
-interface IColProps {
-	key: string;
-	title: string;
-	render?(data: any): ReactElement
+import React, { useState, useMemo } from "react";
+interface Props {
+  data: any[];
+  col: any[];
 }
+const Table = (props: Props) => {
+  const { data, col } = props;
+  const [sortBy, setSortBy] = useState(""); // 排序的字段
+  const [filterBy, setFilterBy] = useState<any>(""); // 输入的字段
+  const [tableData, setTableData] = useState(data);
+  const [searchBy, setSearchBy] = useState<any>(""); // 筛选的字段
 
-interface ITableProps {
-	className?: string;
-	data: Array<any>;
-	cols: Array<IColProps>
-}
+  // 点击表头排序
+  const handleSort = (key: any) => {
+    if (sortBy === key) {
+      // 如果已经是该字段排序，则反向排序
+      setTableData([...data].reverse());
+    } else {
+      // 否则按该字段正向排序
+      setTableData([...data].sort((a, b) => (a[key] > b[key] ? 1 : -1)));
+      setSortBy(key);
+    }
+  };
+  const callback = useMemo(() => {
+    if (searchBy) {
+      setTableData(data.filter((item) => item[col[0]] === Number(searchBy)));
+    } else {
+      setTableData(data);
+    }
+  }, [searchBy]);
+  // 判断输入筛选是否合格
+  const handSearch = (value: String) => {
+    setSearchBy(value);
+  };
+  return (
+    <table>
+      <thead>
+        <tr>
+          {col.map((key) => (
+            <th key={key} onClick={() => handleSort(key)}>
+              {key}
+              {sortBy === key ? "↑" : ""}
+            </th>
+          ))}
+        </tr>
+        <tr>
+          <th colSpan={col.length}>
+            <input
+              placeholder="Search"
+              value={searchBy}
+              onChange={(e) => {
+                handSearch(e.target.value);
+              }}
+            />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {col.map((key, colIndex) => (
+              <td key={colIndex}>{row[key]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-export default function Table(props: ITableProps) {
-	const { data, cols, className } = props;
-
-	const col = useMemo(() => {
-		return cols?.map(c => {
-			return <td key={c?.key}>
-				{c?.title}
-			</td>
-		})
-	}, [cols])
-
-	return <table className={className}>
-		<thead>
-			<tr>{col}</tr>
-		</thead>
-		<tbody>
-			{
-				data?.map(d => {
-					return <tr key={d.id}>
-						{cols?.map(c => {
-							if (c.render) {
-								return <td key={c.key}>
-									{
-										c.render(d)
-									}
-								</td>
-							}
-							return <td key={c.key}>
-								{d[c.key]}
-							</td>
-						})}
-					</tr>
-				})
-			}
-		</tbody>
-	</table>
-}
+export default Table;
